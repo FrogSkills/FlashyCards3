5e88f4b52a9f9b5ba92ba37e24da5598eb9355c1
Miguel <mgamez20@icloud.com>
2026-01-04 13:02:07 -0500

    Initial Commit

---
+++ FlashyCards3/FlashyCards3App.swift
@@ -0,0 +1,17 @@
+//
+//  FlashyCards3App.swift
+//  FlashyCards3
+//
+//  Created by Miguel on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashyCards3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
