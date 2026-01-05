8add3a42a84be12d0f8c08fe7754d11235afc30d
Miguel <mgamez20@icloud.com>
2026-01-04 22:54:29 -0500

    Added the 3 of the main windows that will be needed. Main, individual flashcard set, and to add a new flashcard to the set.

---
+++ FlashyCards3/AddingACard.swift
@@ -0,0 +1,18 @@
+//
+//  AddingACard.swift
+//  FlashyCards3
+//
+//  Created by Miguel on 1/4/26.
+//
+
+import SwiftUI
+
+struct AddingACard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingACard()
+}
---
+++ FlashyCards3/AddingASet.swift
@@ -0,0 +1,18 @@
+//
+//  AddingASet.swift
+//  FlashyCards3
+//
+//  Created by Miguel on 1/4/26.
+//
+
+import SwiftUI
+
+struct AddingASet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddingASet()
+}
